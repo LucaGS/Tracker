e7719c72e501978dbf07a94573de1a965c63e95b
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2025-03-17 19:44:44 +0100

    halldsf

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index(): Response
+    {
+        return new Response('<html><body><h1>Alles läuft!</h1></body></html>');
+    }
+}
