278ea51150eb6c59efa914669dfe450316688861
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2025-03-20 19:38:50 +0100

    fsdf

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,13 +4,14 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
 
 class DefaultController extends AbstractController
 {   
 
     #[Route('/')]
     public function index(): Response
-    {   $content = '<html><body>default: '.$_ENV['token'].'</body></html>';
+    {   $content = '<html><body>default: '.env('token').'</body></html>';
         return new Response(content: $content);
     }
 
