97a4c89769377d4fe62b002e5ae8a79b092ac542
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2025-03-20 19:18:34 +0100

    fafga

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,6 +11,6 @@ class DefaultController extends AbstractController
     #[Route('/')]
     public function index(): Response
     {   $content = '<html><body>default'.$_ENV['env:token'].'</body></html>';
-        return new Response('<html><body>default</body></html>');
+        return new Response(content: $content);
     }
 }
