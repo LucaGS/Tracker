56b97711f324843f10fbcb5ac2682582f93e1a8c
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2025-03-20 19:33:45 +0100

    fadsf

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,7 +10,8 @@ class DefaultController extends AbstractController
 
     #[Route('/')]
     public function index(): Response
-    {   $content = '<html><body>default: '.$_ENV['env:token'].'</body></html>';
+    {   $content = '<html><body>default: '.$_ENV['token'].'</body></html>';
         return new Response(content: $content);
     }
-    }
+
+}
