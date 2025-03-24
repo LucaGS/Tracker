eba114da895f76b8b47244be032f375fd010239c
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2025-03-24 19:17:31 +0100

    more excercise controller stuff

--- src/Controller/ExcerciseController.php
+++ src/Controller/ExcerciseController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Excercise;
 use App\Entity\Test;
 use App\Repository\ExcerciseRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -13,8 +14,33 @@ use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 #[Route('/api/excercise', name: 'api_excercise_')]
 class ExcerciseController extends AbstractController {
     #[Route('/', methods: ['GET'])]
-    public function index(EntityManagerInterface $entityManagerInterface, ExcerciseRepository $repository):JsonResponse{
-        $excercises = $repository->findAll();
+    public function index( ExcerciseRepository $excerciseRepository):JsonResponse{
+        $excercises = $excerciseRepository->findAll();
         return $this->json($excercises);
     }
+    #[Route('/create', methods: ['POST'])]
+    public function createExcercise(Request $request,EntityManagerInterface $entityManager, ExcerciseRepository $excerciseRepository){
+       $data = json_decode($request->getContent(),true);
+
+        if(!isset($data['userid'], $data['trainingplanid'],$data['name'],$data['sets'])){
+            return $this->json(["error"=> "userid, trainplanid, name, sets are required"]);
+        }
+        $excercise = new Excercise();
+        $excercise->setName($data['name']);
+        $excercise->setSets($data['sets']);
+        $excercise->setTrainingplanid($data['trainingplanid']);
+        $excercise->setUserid($data['userid']);
+        $entityManager->persist($excercise);
+        $entityManager->flush();
+        return $this->json($excercise);
+
+    }
+    #[Route('/user/{userid}/{trainingplanid}', methods: ['POST'])]
+    public function getUserExcercises(int $userid ,int $trainingplanid,EntityManagerInterface $entityManager, ExcerciseRepository $excerciseRepository){
+        $excercises = $excerciseRepository->findBy(
+            criteria: ["userid" => $userid,
+        ["trainingplanid"=>$trainingplanid]],);
+        return $this->json($excercises, 201);
+    }
 }
+
