1b73fcc5c5f2cdbee336ee5ad107867b20785976
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2025-04-02 17:59:43 +0200

    started Training Unit Test  and Code

--- src/Controller/StartedtrainingController.php
+++ src/Controller/StartedtrainingController.php
@@ -1,9 +1,9 @@
 <?php
 namespace App\Controller;
-
 use App\Entity\Startedtraining;
 use App\Entity\Trainingplan;
 use App\Repository\StartedtrainingRepository;
+use phpDocumentor\Reflection\DocBlock\Tags\Method;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
@@ -14,6 +14,30 @@ use Symfony\Component\Routing\Annotation\Route;
 use function PHPUnit\Framework\isEmpty;
 #[Route('/api/startedtraining', name: 'api_trainingplan_')]
 class StartedtrainingController extends AbstractController{
+    #[Route("/", methods:['POST'])]
+    public function CreateStartedTrainingplan(Request $request, EntityManagerInterface $entityManager):JsonResponse{
+        $data = json_decode($request->getContent(), true);
+        if (empty($data['userid'])) {
+            return $this->json(['error' => 'User ID is required'], 400);
+        }
 
+        if (empty($data['trainingplanid'])) {
+            return $this->json(['error' => 'Training Plan ID is required'], 400);
+        }
+        $startedTraining = new Startedtraining();
+        $startedTraining->setUserid($data['userid']);
+        $startedTraining->setTrainingplanid($data['trainingplanid']);
+        $entityManager->persist($startedTraining);
+        $entityManager->flush();
+        return $this->json($startedTraining, 201);
+    }
+    #[Route("/{userid}/{trainingplanid}", methods:['GET'])]
+    public function getStartedTrainingPlansByUserIdAndTrainingplanId(StartedtrainingRepository $repository, int $userid, int $trainingplanid):JsonResponse{
+        $startedTraining = $repository->findBy(['userid' => $userid, 'trainingplanid' => $trainingplanid]);
+        if (!$startedTraining) {
+            return $this->json(['error' => 'Started training not found'], 404);
+        }
+        return $this->json($startedTraining);
+    }
     
 }
--- tests/Controller/StartedTrainingControllerTest.php
+++ tests/Controller/StartedTrainingControllerTest.php
@@ -15,42 +15,42 @@ class StartedTrainingControllerTest extends TransactionalTestCase
         $this->client = static::getClient();
     }
 
-    public function testGetStartedTrainingplanById(): void
-    {
-        $startedtraining = new Startedtraining();
-        $startedtraining->setUserid(999999999);
-        $startedtraining->setTrainingplanid(999999999);
-        $this->em->persist($startedtraining);
-        $this->em->flush();
-
-        $this->client->request('GET', '/api/trainingplan/' . $startedtraining->getId());
-        $this->assertResponseIsSuccessful();
-
-        $response = json_decode($this->client->getResponse()->getContent(), true);
-        $this->assertEquals(999999999, $response['trainingplanid']);
-        $this->assertEquals(999999999, $response['userid']);
-    }
-    public function testGetStartedTrainingplanByUserId(): void
+ 
+    public function testGetStartedTrainingplansbyUserIdAndTrainingplanId(): void
     {
         $startedtrainingA = new Startedtraining();
         $startedtrainingA->setUserid(999999999);
         $startedtrainingA->setTrainingplanid(999999999);
         $startedtrainingB = new Startedtraining();
         $startedtrainingB->setUserid(999999999);
-        $startedtrainingB->setTrainingplanid(999999998);
+        $startedtrainingB->setTrainingplanid(999999999);
 
         $this->em->persist($startedtrainingA);
         $this->em->persist($startedtrainingB);
         $this->em->flush();
 
-        $this->client->request('GET', '/api/trainingplan/'. $startedtrainingA->getUserid());
+        $this->client->request('GET', '/api/startedtraining/'. $startedtrainingA->getUserid()."/".$startedtrainingA->getTrainingplanid());
         $response = json_decode($this->client->getResponse()->getContent(), true);
        
         $this->assertResponseIsSuccessful();
         $this->assertCount(2, $response);
         $trainingplanIds = array_column($response, 'trainingplanid');
         $this->assertContains(999999999, $trainingplanIds);
-        $this->assertContains(999999998, $trainingplanIds);
+        $this->assertContains(999999999, $trainingplanIds);
 
+    }
+    public function testCreateStartedTrainingplan():void{
+        $this->client->request('POST', '/api/startedtraining/', [], [], [
+            'CONTENT_TYPE' => 'application/json',
+        ], json_encode([
+            'userid' => 99999,
+            'trainingplanid' => 99999,
+        ]));
+        //$this->assertResponseStatusCodeSame(201);
+        $response = json_decode($this->client->getResponse()->getContent(), true);
+        $this->assertResponseStatusCodeSame(201);
+        $this->assertArrayHasKey('id', $response);
+        $this->assertEquals(99999, $response['userid']);
+        $this->assertEquals(99999, $response['trainingplanid']);
     }
 }
